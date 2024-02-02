97a5b74f7eb32700e0474225d75eaa2de9287204
Keith Perkins <keith.perkins@cnu.edu>
2024-02-02 00:50:34 -0500

    lecture

--- week_4/preprocess_demo.py
+++ week_4/preprocess_demo.py
@@ -19,7 +19,6 @@ if (PROJECT_ROOT not in sys.path):
     sys.path.append(PROJECT_ROOT)
 
 import utils as ut
-from utils.transforms import *
 
 # every python file has a name
 # if this file is running as a script (python3 preprocess_demo.py) then it's name is __main__
@@ -27,13 +26,13 @@ from utils.transforms import *
 # the code below only runs when running as a script
 if __name__== "__main__":
     #load raw t-shirt order
-    df = ut.getdata.generate_tshirt_order(100,100,100,dups=100, percent_nans=0.2)
+    df = ut.generate_tshirt_order(100,100,100,dups=100, percent_nans=0.2)
+    df.iloc[1,3]='"-,.."'
 
     # this is the hand coded bit for nominal cat var
     vals ={'t_shirt_size': {'large': 2, 'medium': 1, 'small': 0}}
-
-    #run a pipeline of transforms
-    df=run_pipeline(df,dup_features=['name'], dummy_features=['t_shirt_color'], ordinal_features=['t_shirt_size'], ordering_dict=vals)
-
     
+    #run a pipeline of transforms, note all functions are from ut namespace
+    df=df.pipe(ut.impute_NaNs).pipe(ut.ps_lower_strip).pipe(ut.ps_replace_punctuation,['name']).pipe(ut.remove_duplicates,['name']).pipe(ut.cat_ordinal,['t_shirt_size'],vals).pipe(ut.drop_no_variance_columns).pipe(ut.scale).pipe(ut.cat_getdummies, ['t_shirt_color']).pipe(ut.drop_correlated_columns)
+
     df.to_feather('./script.feather')
