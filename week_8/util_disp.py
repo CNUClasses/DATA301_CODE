1b8b4a709e993bdea44bfe83ec7a981cea1f996b
Keith Perkins <keith.perkins@cnu.edu>
2024-03-26 23:43:54 -0400

    lecture

--- week_8/util_disp.py
+++ week_8/util_disp.py
@@ -3,11 +3,9 @@ import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
 def plot_decision_regions(X, y, classifier, test_idx=None, resolution=0.02):
 
-    # setup marker generator and color map
+    # setup marker generator 
     markers = ('s', 'x', 'o', '^', 'v')
-    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
-    cmap = ListedColormap(colors[:len(np.unique(y))])
-
+   
     # plot the decision surface
     x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
     x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
@@ -15,7 +13,7 @@ def plot_decision_regions(X, y, classifier, test_idx=None, resolution=0.02):
                            np.arange(x2_min, x2_max, resolution))
     Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
     Z = Z.reshape(xx1.shape)
-    plt.contourf(xx1, xx2, Z, alpha=0.4, cmap=cmap)
+    plt.contourf(xx1, xx2, Z, alpha=0.4)
     plt.xlim(xx1.min(), xx1.max())
     plt.ylim(xx2.min(), xx2.max())
 
